b8b9d0de58567408d7430d64de86f64f4bfef7e7
unknown <sistemas@VRE_LAB0204.inacap.cl>
2022-03-15 21:33:34 -0300

    ejercicios en clase

---
+++ ejercicio.py
@@ -0,0 +1,7 @@
+numeroNotas = int(input("ingrese el numero de notas a calcular\n"))
+cont=1
+notas=0
+while cont <= numeroNotas:
+    notas+=int(input(f"ingrese nota {cont}\n"))
+    cont+=1
+print(f"el promedio es {notas/numeroNotas}")
